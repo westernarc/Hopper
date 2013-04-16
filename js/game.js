071f8f93d1e876f28edf7abcb708bb5de814f748
westernarc <deepspace30@gmail.com>
2013-04-16 19:23:08 -0400

    Fixed collision

--- js/game.js
+++ js/game.js
@@ -9,7 +9,7 @@ var x = 150;
 var y = 150;
 var dx = 2;
 var dy = 4;
-var playerRadius = 100;
+var playerRadius = 150;
 
 //Frame array
 var imgObj;
@@ -36,9 +36,12 @@ var platforms;
 var platformWidth = 50;
 var platformHeight = 15;
 
+var imgFloor;
+var imgCloud;
+
 var floorY;
 
-var platformBaseWidth = 100;
+var platformBaseWidth = 140;
 var platformMinimumWidth = 40;
 // ----------------------------------------
 
@@ -55,10 +58,10 @@ function init() {
 	width = canvas.width;
 	height = canvas.height;
 	
-	floorY = height * 5 / 6;
+	floorY = height * 4 / 6;
 	y = floorY;
 	
-	platforms = [{x:0, y: height - platformHeight, platformwidth:width}];
+	platforms = [{x:0, y: height - platformHeight * 3, platformwidth:width}];
 	
 	varLevel = 0;
 	updatePlatforms();
@@ -76,49 +79,69 @@ function init() {
 	currentFrame = 1;
 	
 	imgBackdrop1 = new Image();
-	imgBackdrop1.src = 'img/backdrop.jpg';
+	imgBackdrop1.src = 'img/backdrop.png';
 	backdropY1 = height - imgBackdrop1.height * 2;
 	
 	imgBackdrop2 = new Image();
-	imgBackdrop2.src = 'img/backdrop.jpg';
+	imgBackdrop2.src = 'img/backdrop.png';
 	backdropY2 = height - imgBackdrop2.height;
+	
+	imgFloor = new Image();
+	imgFloor.src = 'img/floor.png';
+	
+	imgCloud = new Image();
+	imgCloud.src = 'img/cloud.png';
+}
+
+function reinit() {
 }
 
 function updatePlatforms() {
 	while(platforms.length < 6) {
-		var pwidth = Math.max(platformBaseWidth - (varLevel * 5), platformMinimumWidth);
+		var pwidth = Math.max(platformBaseWidth - (varLevel / 200), platformMinimumWidth);
 		platforms.push({x:Math.random() * (width - pwidth), y: height - platformHeight - platforms.length * 200, platformwidth:pwidth});
 	}
 }
 function draw() {
+	//Clear
 	ctx.clearRect(0, 0, width, height);
 
+	//Draw background
 	ctx.drawImage(imgBackdrop1, 0, backdropY1);
 	ctx.drawImage(imgBackdrop2, 0, backdropY2);
+	
+	
+	//Draw the player
+	ctx.fillStyle = "#00A308";
+	ctx.beginPath();
+	//ctx.fillRect(x, y, playerRadius, playerRadius);
+	ctx.closePath();
 	
 	ctx.drawImage(imgObj[Math.round(currentFrame)], x, y, playerRadius, playerRadius);
 	
 	//Update animation frame
 	if(currentFrame < frameCount) {
 		currentFrame += 0.15;
+		if(currentFrame > frameCount) {
+			currentFrame = frameCount;
+		}
 	}
 
-	ctx.fillStyle = "#00A308";
-	ctx.beginPath();
-	//ctx.fillRect(x, y, playerRadius, playerRadius);
-	ctx.closePath();
-	ctx.fill();
-	
 	//Draw the platforms
 	for(var i = 0; i < platforms.length; i++) {
 		ctx.beginPath();
-		ctx.fillRect(platforms[i].x, platforms[i].y, platforms[i].platformwidth, platformHeight);
+		if(platforms[i].platformwidth > 200) {
+			ctx.drawImage(imgFloor, platforms[i].x, platforms[i].y, platforms[i].platformwidth, imgFloor.height);
+		} else {
+			ctx.drawImage(imgCloud, platforms[i].x, platforms[i].y, platforms[i].platformwidth, (platforms[i].platformwidth / imgCloud.width) * imgCloud.height);
+		}
+		//ctx.fillRect(platforms[i].x, platforms[i].y, platforms[i].platformwidth, platformHeight);
 		ctx.closePath();
 	}
 	
 	//Collisions against platforms
 	for(var i = 0; i < platforms.length; i++) {
-		if(Math.abs((x + playerRadius/2) - (platforms[i].x + platforms[i].platformwidth/2)) < (platforms[i].platformwidth/2 + playerRadius / 8) && Math.abs(y - (platforms[i].y - platformHeight * 3)) < platformHeight && dy > 0) {			
+		if(Math.abs((x + playerRadius/2) - (platforms[i].x + platforms[i].platformwidth/2)) < (platforms[i].platformwidth/2 + playerRadius / 8) && Math.abs((y + playerRadius/2) - (platforms[i].y)) < 5 && dy > 0) {			
 			dy = -8;
 			currentFrame = 1;
 		}
@@ -130,9 +153,10 @@ function draw() {
 	dy += 0.1;	
 	
 	//If the player goes above 1/2 screen, move all of the pieces down
-	if(y < height / 2) {
+	if(y < height / 3) {
 		if(dy < 0) {
 			y -= dy;
+			varLevel -= Math.round(dy);
 			for(var i = 0; i < platforms.length; i++) {
 				platforms[i].y -= dy;
 			}
@@ -149,22 +173,28 @@ function draw() {
 			
 			if(platforms[0].y > height) {
 				platforms.shift();
-				varLevel += 1;
 				updatePlatforms();
 			}
 		}
 	}
+	
+	grad = ctx.createLinearGradient(0, 0, 0, 40);
+	grad.addColorStop(0, "#333");
+	grad.addColorStop(1, "#111");
+	ctx.fillStyle = grad;
+	ctx.beginPath();
+	ctx.fillRect(0, 0, width, 50);
+	ctx.closePath();
+	ctx.fill();
+	
+	ctx.fillStyle = "#ddd";
+	ctx.font = "24px Arial";
+	ctx.fillText(varLevel, 15, 35);
 }
 
 function trackPosition(e) {
 	mouse = getMousePos(canvas, e);
 	x = mouse.x - playerRadius/2;
-		
-	if(x > width - playerRadius) {
-		x = width - playerRadius;
-	} else if(x < 0) {
-		x = 0;
-	}
 }
 function getMousePos(canvas, event) {
 	var rect = canvas.getBoundingClientRect();
